4e1dc28aa1de39441782b4fd0d02a7be1dd3b4a8
pfviktor <viktocode@gmail.com>
2022-12-01 12:53:04 +0100

    dropdown menu ready genre updates

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ startMenu.style.width = "20%";
 startMenu.classList.add("center");
 startMenu.style.marginTop = "20%";
 
+//DROPDOWN MENU
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
 //DRAG & DROP
 
 /* let trapDiv = document.createElement("div");
@@ -73,18 +96,61 @@ function dragend_handler(ev) {
 
 //DRUM SAMPLES & STYLES
 
-let audio_kick = new Audio("kick_trap.wav");
+let audio_kick = new Audio();
 
-let audio_snare = new Audio("snare_trap.wav");
+let audio_snare = new Audio();
 
-let audio_hihat = new Audio("hihat_trap.wav");
+let audio_hihat = new Audio();
 
-let audio_crash = new Audio("crash_trap.wav");
+let audio_crash = new Audio();
+
+//OPTIONS
+
+const optionRock = document.getElementById("optionRock");
+const optionTrap = document.getElementById("optionTrap");
+const optionTribal = document.getElementById("optionTribal");
+const optionLoFi = document.getElementById("optionLoFi");
+
+optionTrap.addEventListener("click", () => {
+  audio_kick = new Audio("kick_trap.wav");
+
+  audio_snare = new Audio("snare_trap.wav");
+
+  audio_hihat = new Audio("hihat_trap.wav");
+
+  audio_crash = new Audio("crash_trap.wav");
+});
+
+
+optionLoFi.addEventListener("click", () => {
+  audio_kick = new Audio("kick_lofi.wav");
+
+  audio_snare = new Audio("snare_lofi.wav");
+
+  audio_hihat = new Audio("hihat_lofi.wav");
+
+  audio_crash = new Audio("crash_lofi.wav");
+});
+
+optionTribal.addEventListener("click", () => {
+  audio_kick = new Audio("kick_tribal.wav");
+
+  audio_snare = new Audio("snare_tribal.wav");
+
+  audio_hihat = new Audio("hihat_tribal.wav");
+
+  audio_crash = new Audio("crash_tribal.wav");
+});
+
+// START !!!
+
+let dropDownMenu = document.querySelector(".dropbtn");
 
 startMenu.addEventListener("click", () => {
   let openingFill = new Audio("openingfill.wav");
   openingFill.play();
   document.body.removeChild(startDiv);
+  dropDownMenu.style.visibility = "visible";
 
   //DIV ELEMENT
   const divElement = document.createElement("div");
@@ -96,6 +162,13 @@ startMenu.addEventListener("click", () => {
   divElement.appendChild(h1Title);
   h1Title.style.textAlign = "center";
   h1Title.style.fontSize = "xxx-large";
+
+  //H2 ELEMENT
+  const h2Title = document.createElement("h2");
+  h2Title.innerHTML = "CHOOSE A GENRE";
+  divElement.appendChild(h2Title);
+  h1Title.style.fontSize = "xx-large";
+  h2Title.style.marginLeft = "790px"
 
   //FOUR BUTTONS
 
